c518e64dea883d58bd004e97a34dd8890083fdb2
madhavan.anand@gmail.com <madhavan.anand@gmail.com@9b4a8194-3dcd-11de-9d05-31133e6853b1>
2009-05-11 02:17:17 +0000

    paper version commit.

---
+++ trunk/sparse_coding/cuda/paper_demo.sh
@@ -0,0 +1,3 @@
+make
+./cl1ls -mb=1000 -nb=5 -k=1024 -n=196 -nepoch=3 -imagesdir=../../../images -runwhat=natural -basisdir=tmp -beta=0.5 -nbasis_iters=30 -eta=0.04
+
---
+++ trunk/sparse_coding/cuda/utils.hh
@@ -0,0 +1,25 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include "cutil.h"
+#include <string>
+#include "coreutils.hh"
+
+namespace cpu {
+class CpuEventTimer {
+  float& _time;
+  unsigned int timer;
+  public:
+  CpuEventTimer(float& time):_time(time), timer(0) { 
+    CUDA_SAFE_CALL( cutCreateTimer(&timer) );
+    CUDA_SAFE_CALL( cutStartTimer(timer) );
+  }
+  ~CpuEventTimer() { 
+    CUDA_SAFE_CALL( cutStopTimer(timer) );
+    _time = cutGetTimerValue(timer);
+    CUT_SAFE_CALL(cutDeleteTimer(timer));
+  }
+};
+}
+
+#endif
